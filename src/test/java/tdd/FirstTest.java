81da72dc4474c63c655c256ff3cc12196d1ba89d
jarvis <arahansa@naver.com>
2016-04-30 12:52:51 +0900

    7장 사과와 오렌지 - Franc와 Orange 의 비교를 하였다.

--- src/test/java/tdd/FirstTest.java
+++ src/test/java/tdd/FirstTest.java
@@ -26,6 +26,7 @@ public class FirstTest {
         assertFalse(new Dollor(5).equals(new Dollor(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollor(5)));
     }
 
     @Test
